951c2fe483748dfff417ffbe492b71c726a71dab
Chinwenma <okoriechinwe1327@gmail.com>
2024-01-25 11:39:27 -0800

    editing

--- src/Components/RandomQuote/RandomQuote.jsx
+++ src/Components/RandomQuote/RandomQuote.jsx
@@ -1,15 +1,15 @@
+
 import React, { useState, useEffect } from 'react';
 import './RandomQuote.css';
 import twitter from '../Assests/x.png';
 import reload from '../Assests/reload.png';
 
 const RandomQuote = () => {
+  const [quotes, setQuotes] = useState([]);
   const [quote, setQuote] = useState({
     text: "Difficulties increase the nearer we get to the goal",
     author: "Johann Wolfgang von Goethe"
   });
-
-  const [quotes, setQuotes] = useState([]);
 
   useEffect(() => {
     async function loadQuotes() {
@@ -18,17 +18,12 @@ const RandomQuote = () => {
       setQuotes(data);
     }
 
-    const openTwitter = ()=> {
-      window.open(`https://twitter.com/intent/tweet?text=${quote.text} - ${quote.author.split(',')[0]}`);
-    }
     loadQuotes();
   }, []);
 
   const random = () => {
-    const select = quotes[Math.floor(Math.random() * quotes.length)];
-    // Handle the case where the author is "type.fit"
-    const selectedAuthor = select.author === "type.fit" ? "Unknown" : select.author;
-    setQuote({ text: select.text, author: selectedAuthor });
+    const selectedQuote = quotes[Math.floor(Math.random() * quotes.length)];
+    setQuote(selectedQuote);
   };
 
   return (
@@ -39,10 +34,10 @@ const RandomQuote = () => {
       <div>
         <div className='line'></div>
         <div className='bottom'>
-          <div className='author'>- {quote.author.split(',')[0]}</div>
+          <div className='author'>{quote.author ? `-${quote.author}` : 'unknown'}</div>
           <div className='icons'>
-            <img src={reload} onClick={() => {random()}} alt='' />
-            <img src={twitter}onClick={() => {openTwitter()}} alt='' />
+            <img src={reload} onClick={random} alt='' />
+            <img src={twitter} alt='' />
           </div>
         </div>
       </div>
@@ -51,3 +46,4 @@ const RandomQuote = () => {
 };
 
 export default RandomQuote;
+
